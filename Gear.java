81f1f379a50a876f946ee8650a4e252e0243d9cf
2010shagun <52991393+2010shagun@users.noreply.github.com>
2020-07-12 20:20:58 +0530

    Inner class wheel created
    Gear.java updated
         New inner class wheel created 
                Data members - rim, tire
                Getter implemented
                diameter function implemented to return the value

--- Gear.java
+++ Gear.java
@@ -1,23 +1,41 @@
 
 public class Gear {
-	double chainring, cog, rim, tire;
+	double chainring, cog;
+	Wheel wheel;
+	
 	public Gear(double chainring, double cog, double rim, double tire) {
 		this.chainring=chainring;
 		this.cog=cog;
-		this.rim=rim;
-		this.tire=tire;
+		wheel = new Wheel(rim,tire);
 	}
+	
 	public double getCog() { return cog; }
 	public double getChainring() { return chainring; }
-	public double getRim() { return rim;}
-	public double getTire() { return tire;}
 	
 	public double ratio() {
 		return getChainring()/getCog();
 	}
+	
 	public double gear_inches() {
-		return (ratio()*(getRim()+(getTire()*2)));
+		return (ratio()*(wheel.diameter()));
 	}
+	
+	class Wheel
+	{
+		double rim, tire;
+		public Wheel(double rim, double tire)
+		{
+			this.rim=rim;
+			this.tire=tire;
+		}
+		public double getRim() { return rim;}
+		public double getTire() { return tire;}
+		public double  diameter() {
+			return getRim()+getTire()*2;
+		}
+	}
+	
+		
 	public static void main(String args[]) {
 		Gear gear1=new Gear(52,11,26,1.5);
 		Gear gear2= new Gear(52, 11, 24, 1.25);
