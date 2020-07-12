6561e98323512d1be71ca5ad6a347e7e329aa76a
2010shagun <52991393+2010shagun@users.noreply.github.com>
2020-07-12 18:18:31 +0530

    gear_inches() implemented in Gear.java
    Gear.java file updated.
    gear_inches() function implemented to to compare bicycles that differ in both gearing and wheel size. The formula follows:
    gear inches = wheel diameter * gear ratio
    where
    wheel diameter = rim diameter + twice tire diameter.

--- Gear.java
+++ Gear.java
@@ -1,17 +1,22 @@
 
 public class Gear {
-	double chainring, cog;
-	public Gear(double chainring, double cog) {
+	double chainring, cog, rim, tire;
+	public Gear(double chainring, double cog, double rim, double tire) {
 		this.chainring=chainring;
 		this.cog=cog;
+		this.rim=rim;
+		this.tire=tire;
 	}
 	public double ratio() {
 		return chainring/cog;
 	}
+	public double gear_inches() {
+		return (ratio()*(rim+(tire*2)));
+	}
 	public static void main(String args[]) {
-		Gear gear1=new Gear(52,11);
-		Gear gear2= new Gear(30, 27);
-		System.out.println(gear1.ratio());
-		System.out.println(gear2.ratio());
+		Gear gear1=new Gear(52,11,26,1.5);
+		Gear gear2= new Gear(52, 11, 24, 1.25);
+		System.out.println(gear1.gear_inches());
+		System.out.println(gear2.gear_inches());
 	}
 }
