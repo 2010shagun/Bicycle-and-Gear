394c852a9eb193e77474221955cb0f33c6685504
2010shagun <52991393+2010shagun@users.noreply.github.com>
2020-07-12 22:00:03 +0530

    Dependency Injection. Decoupling by rearranging existing code.
    removed hard-coded  reference to Wheel deep inside Gear's gear_inches method
    reason - It is not necessary for Gear to know about the existence of the Wheel class in order to calculate gear_inches. 
    It doesn’t need to know that Wheel expects to be initialized with a rim and then a tire; it just needs
    an object that knows diameter.
    Gear is now smarter because it knows less

--- Gear.java
+++ Gear.java
@@ -19,30 +19,29 @@ class Wheel
 }
 
 public class Gear {
-	double chainring, cog, rim, tire;
-	public Gear(double chainring, double cog, double rim, double tire) {
+	double chainring, cog;
+	Wheel wheel;
+
+	public Gear(double chainring, double cog, Wheel wheel) {
 		this.chainring=chainring;
 		this.cog=cog;
-		this.rim=rim;
-		this.tire=tire;
+		this.wheel = wheel;
 	}
-	
+
 	public double getCog() { return cog; }
 	public double getChainring() { return chainring; }
-	public double getRim() { return rim;}
-	public double getTire() { return tire; }
 	
 	public double ratio() {
 		return getChainring()/getCog();
 	}
 	
 	public double gear_inches() {
-		return (ratio()*(new Wheel(rim, tire).diameter()));
+		return (ratio()*(wheel.diameter()));
 	}
 	
 	public static void main(String args[]) {
 		
-		Gear gear1=new Gear(52,11,26,1.5);
+		Gear gear1=new Gear(52,11,new Wheel(26,1.5));
 		System.out.println(gear1.gear_inches());
 	}
 }
