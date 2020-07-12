98be73e5a84b8d2b31bd7100567aacffe7186020
2010shagun <52991393+2010shagun@users.noreply.github.com>
2020-07-12 20:43:45 +0530

    Wheel class made independent
    Updated Gear.java
    Inner class Wheel made independent
    new function of circumference added in wheel class
    In Gear class, another constructor added

--- Gear.java
+++ Gear.java
@@ -1,12 +1,34 @@
 
+
+class Wheel
+{
+	double rim, tire;
+	public Wheel(double rim, double tire) {
+		this.rim=rim;
+		this.tire=tire;
+	}
+	public double getRim() { return rim;}
+	public double getTire() { return tire;}
+	public double  diameter() {
+		return getRim()+getTire()*2;
+	}
+	public double circumference() {
+		return diameter()*Math.PI;
+	}
+	
+}
+
 public class Gear {
 	double chainring, cog;
 	Wheel wheel;
-	
-	public Gear(double chainring, double cog, double rim, double tire) {
+	public Gear(double chainring, double cog) {
 		this.chainring=chainring;
 		this.cog=cog;
-		wheel = new Wheel(rim,tire);
+	}
+	public Gear(double chainring, double cog, Wheel wheel) {
+		this.chainring=chainring;
+		this.cog=cog;
+		this.wheel = wheel;
 	}
 	
 	public double getCog() { return cog; }
@@ -20,26 +42,18 @@ public class Gear {
 		return (ratio()*(wheel.diameter()));
 	}
 	
-	class Wheel
-	{
-		double rim, tire;
-		public Wheel(double rim, double tire)
-		{
-			this.rim=rim;
-			this.tire=tire;
-		}
-		public double getRim() { return rim;}
-		public double getTire() { return tire;}
-		public double  diameter() {
-			return getRim()+getTire()*2;
-		}
-	}
-	
-		
 	public static void main(String args[]) {
-		Gear gear1=new Gear(52,11,26,1.5);
-		Gear gear2= new Gear(52, 11, 24, 1.25);
+		Wheel wheel1=new Wheel(26,1.5);
+		Gear gear1=new Gear(52,11,wheel1);
+		Gear gear2= new Gear(52, 11);
+		System.out.println(wheel1.circumference());
 		System.out.println(gear1.gear_inches());
-		System.out.println(gear2.gear_inches());
+		System.out.println(gear2.ratio());
 	}
 }
+	
+
+	
+		
+	
+
